6933dd0efd6afe9be5316dc0ef2acb632d17052c
Nabin <imnabn@gmail.com>
2018-02-20 21:25:11 -0600

    Update parenthesis-checker.py

--- ADTs/Stack/parenthesis-checker.py
+++ ADTs/Stack/parenthesis-checker.py
@@ -1,4 +1,3 @@
-
 from stack import Stack
 
 def par_checker(symbol_string):
@@ -9,13 +8,23 @@ def par_checker(symbol_string):
 	return true if stack is empty at the end
 	otherwise return false
 	"""	
-	s = Stack()
 	
+	# create a mapping for opening and closing baraces
+	opening_braces = [ch for ch in "({["]
+	closing_barces = [ch for ch in ")}]"]
+	
+	match = {k:v for k, v in zip(opening_braces,closing_barces)}
+
+	s = Stack()
 	for symbol in symbol_string:
-		if symbol == "(": 
+		
+		if symbol in opening_braces: 
 			s.push(symbol)
-		elif symbol == ")":
-			s.pop()
+
+		elif symbol in closing_barces:
+			if match[s.pop()] != symbol:
+				return False
+
 		else:
 			continue
 
@@ -30,3 +39,6 @@ if __name__ == '__main__':
 	print(par_checker('(()')) # False
 	print(par_checker('1+(2+3*(9-6))-3')) # True
 	print(par_checker('1+(2+3*(9-6)-3')) # False
+
+	print(par_checker('{{([][])}()}')) # True
+	print(par_checker('[{()]')) # False
