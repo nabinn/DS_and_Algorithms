60bd0484caf0caacde0253dc9335892f698978a5
nabinn <imnabn@gmail.com>
2018-02-21 16:11:26 -0600

    update gitignore

--- ADTs/Stack/stack.py
+++ ADTs/Stack/stack.py
@@ -1,39 +1,39 @@
 class Stack:
-	"""
-	Stack implementation using list.
-	The end of the list is considered top.
-	All operations listed below are O(1)
-	"""
-	def __init__(self):
-		self.items = []
+    """
+    Stack implementation using list.
+    The end of the list is considered top.
+    All operations listed below are O(1)
+    """
 
-	def isEmpty(self):
-		return self.items == []
+    def __init__(self):
+        self.items = []
 
-	def push(self, item):
-		self.items.append(item)
+    def isEmpty(self):
+        return self.items == []
 
-	def pop(self):
-		return self.items.pop()
+    def push(self, item):
+        self.items.append(item)
 
-	def peek(self):
-		return self.items[len(self.items)-1]
+    def pop(self):
+        return self.items.pop()
 
-	def size(self):
-		return len(self.items)
+    def peek(self):
+        return self.items[len(self.items) - 1]
 
+    def size(self):
+        return len(self.items)
 
 
 if __name__ == '__main__':
-	s=Stack()
-	print(s.isEmpty())
-	s.push(4)
-	s.push('dog')
-	print(s.peek())
-	s.push(True)
-	print(s.size())
-	print(s.isEmpty())
-	s.push(8.4)
-	print(s.pop())
-	print(s.pop())
-	print(s.size())
+    s = Stack()
+    print(s.isEmpty())
+    s.push(4)
+    s.push('dog')
+    print(s.peek())
+    s.push(True)
+    print(s.size())
+    print(s.isEmpty())
+    s.push(8.4)
+    print(s.pop())
+    print(s.pop())
+    print(s.size())
