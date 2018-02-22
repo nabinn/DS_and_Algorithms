a055312c99edd6f41c58f2082196498f29998ed3
nabinn <imnabn@gmail.com>
2018-02-22 11:37:05 -0600

    implement simple queue

---
+++ ADTs/Queue/queue.py
@@ -0,0 +1,36 @@
+class Queue():
+    """Simple queue implementation using python list.
+    Assuming rear is at position 0 of the list and front
+    is at the last position. Hence, in this implementation,
+    enqueue is O(n) and dequeue is O(1).
+    """
+
+    def __init__(self):
+        self.items = []
+
+    def is_empty(self):
+        return self.items == []
+
+    def enqueue(self, item):
+        self.items.insert(0, item)
+
+    def dequeue(self):
+        return self.items.pop()
+
+    def size(self):
+        return len(self.items)
+
+
+if __name__ == "__main__":
+    q = Queue()
+    print("size:",q.size())
+    print("is_empty: ", q.is_empty())
+    q.enqueue(5)
+    q.enqueue('apple')
+    q.enqueue(True)
+    print("size: ", q.size())
+    print("is_empty: ", q.is_empty())
+    q.enqueue(9.3)
+    print(q.dequeue())
+    print(q.dequeue())
+    print(q.size())
