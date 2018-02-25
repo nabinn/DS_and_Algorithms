f05a95cb6e2939652715a14d168f6c24f37a5c8c
nabinn <imnabn@gmail.com>
2018-02-24 18:27:45 -0600

    write node class in a separate file

--- ADTs/LinkedList/linked_list.py
+++ ADTs/LinkedList/linked_list.py
@@ -1,22 +1,4 @@
-class Node:
-    """A simple node with data field and next pointer
-    """
-
-    def __init__(self, data):
-        self.data = data
-        self.next = None
-
-    def get_data(self):
-        return self.data
-
-    def get_next(self):
-        return self.next
-
-    def set_data(self, new_data):
-        self.data = new_data
-
-    def set_next(self, new_next):
-        self.next = new_next
+from simple_node import Node
 
 
 class LinkedList:
---
+++ ADTs/LinkedList/simple_node.py
@@ -0,0 +1,19 @@
+class Node:
+    """A simple node with data field and next pointer
+    """
+
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+    def get_data(self):
+        return self.data
+
+    def get_next(self):
+        return self.next
+
+    def set_data(self, new_data):
+        self.data = new_data
+
+    def set_next(self, new_next):
+        self.next = new_next
