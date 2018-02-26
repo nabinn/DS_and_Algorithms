cad95af170a390143dd2156508d38d19e9625fd1
nabinn <imnabn@gmail.com>
2018-02-26 16:12:40 -0600

    add stack based base conversion

--- Recursion/number_base_conversion.py
+++ Recursion/number_base_conversion.py
@@ -1,3 +1,8 @@
+import sys
+sys.path.append('../ADTs/Stack')
+
+from stack import Stack
+
 
 def base_conversion(n, base):
     """converts a decimal number to any
@@ -10,7 +15,27 @@ def base_conversion(n, base):
     if n < base:
         return num_str[n]
     else:
-        return base_conversion(n//base, base) + num_str[n % base]
+        return base_conversion(n // base, base) + num_str[n % base]
+
+
+def base_conversion_stack(n, base):
+    """converts a decimal number to any base between 2 and 16.
+    This implementation makes use of stack instead of recursion.
+    :param: n(int) is a decimal number
+    Returns the converted number as a string."""
+    num_stack = Stack()
+    num_str = "0123456789ABCDEF"
+
+    while n > 0:
+        if n < base:
+            num_stack.push(num_str[n])
+        else:
+            num_stack.push(num_str[n % base])
+        n = n // base
+    result = ""
+    while not num_stack.is_empty():
+        result += num_stack.pop()
+    return result
 
 
 if __name__ == "__main__":
@@ -18,3 +43,7 @@ if __name__ == "__main__":
     print(base_conversion(num, 2))  # converting to binary
     print(base_conversion(num, 8))  # converting to octal
     print(base_conversion(num, 16))  # to hexadecimal
+
+    print(base_conversion_stack(num, 2))  # converting to binary
+    print(base_conversion_stack(num, 8))  # converting to octal
+    print(base_conversion_stack(num, 16))  # to hexadecimal
