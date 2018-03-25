886c16a76721f89c6538108e33eca147d0827bf6
nabinn <imnabn@gmail.com>
2018-03-25 09:54:35 -0500

    add recursive binary search

--- SearchingAlgorithms/binary_search.py
+++ SearchingAlgorithms/binary_search.py
@@ -41,10 +41,36 @@ def recursive_binary_search(num_list, item):
                 return recursive_binary_search(num_list[:mid], item)
 
 
-if __name__ == "__main__":
-    test_list2 = [-34, -8, 0, 23, 50, 56, 78, 100]
-    # print(binary_search(test_list2, 23))
-    # print(binary_search(test_list2, 101))
+def recursive_binary_search_v2(num_list, item):
+    """alternate version of recursive binary search.
+    This invokes rec_bin_search() which does the actual work.
+    """
 
-    print(recursive_binary_search(test_list2, 23))
-    print(recursive_binary_search(test_list2, 101))
+    def rec_bin_search(number_list, key, low, high):
+        if low > high:
+            return False
+        else:
+            mid = (low + high) // 2
+
+            if number_list[mid] == key:
+                return True
+            else:
+                if key > number_list[mid]:
+                    return rec_bin_search(number_list, key, mid + 1, high)
+                else:
+                    return rec_bin_search(number_list, key, low, mid - 1)
+
+    return rec_bin_search(num_list, item, 0, len(num_list)-1)
+
+
+if __name__ == "__main__":
+    list_of_numbers = [-34, -8, 0, 23, 50, 56, 78, 100]
+
+    print(binary_search(list_of_numbers, 23))
+    print(binary_search(list_of_numbers, 101))
+
+    print(recursive_binary_search(list_of_numbers, 23))
+    print(recursive_binary_search(list_of_numbers, 101))
+
+    print(recursive_binary_search_v2(list_of_numbers, 23))
+    print(recursive_binary_search_v2(list_of_numbers, 101))
