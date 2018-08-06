29545ca9e97f3a55e725da348a8acb454788367e
Nabin <imnabn@gmail.com>
2018-08-05 22:12:26 -0400

    7. Reverse Integer

--- AlgorithmProblems/leetcode_007.py
+++ AlgorithmProblems/leetcode_007.py
@@ -5,23 +5,18 @@
 def reverse_number(x):
     result = 0
     is_positive = 1
-
     # for 32 bit number
     low = -2 ** 31
     high = 2 ** 31 - 1
-
     # for negative number
     if x < 0:
         is_positive = -1
         x = -1 * x
-
     while x != 0:
         result = result * 10 + x % 10
-
         if result > high or result < low:
             return 0
         x //= 10
-
     return result * is_positive
 
 
@@ -32,12 +27,10 @@ def reverse(input_num):
     """
     if input_num < 0:
         return -reverse(-input_num)
-
     result = 0
     while input_num > 0:
         result = result * 10 + input_num % 10
         input_num = input_num // 10
-
     return result if result <= 0x7fffffff else 0
 
 
