3fc43eecfe4456764967eade3361eed61d3a4d28
nabinn <imnabn@gmail.com>
2018-08-12 00:19:15 -0400

    add tail recursive factorial function

--- Recursion/factorial.py
+++ Recursion/factorial.py
@@ -7,6 +7,19 @@ def factorial(n):
         return n * factorial(n-1)
 
 
+def tail_factorial(n, accumulator=1):
+    """Tail recursive factorial"""
+    if n <= 0:
+        return accumulator
+    else:
+        return tail_factorial(n-1, accumulator * n)
+
+
 if __name__ == "__main__":
     print(factorial(5))
+    print(factorial(4))
     print(factorial(0))
+
+    print(tail_factorial(5))
+    print(tail_factorial(4))
+    print(tail_factorial(0))
