bddbedb8e76ce563324e1eeca823d83490f1eeaf
nabinn <imnabn@gmail.com>
2018-08-19 13:28:41 -0400

    add mergesort description

--- SortingAlgorithms/mergesort.py
+++ SortingAlgorithms/mergesort.py
@@ -1,3 +1,15 @@
+"""
+Merge sort is a recursive algorithm that continually splits a list in half.
+If the list is empty or has one item, it is sorted by definition (the base case).
+If the list has more than one item, we split the list and recursively invoke a
+merge sort on both halves.
+
+Once the two halves are sorted, the fundamental operation, called a merge, is performed.
+Merging is the process of taking two smaller sorted lists and combining them together
+into a single, sorted, new list.
+"""
+
+
 def merge(left_array, right_array):
     """Takes in two sorted arrays and merges them such
     that the resultant array is sorted"""
